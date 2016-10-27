eab000e5f2a9c1752b05d918292b1e447d362467
xMonkIT <omelin.vladimir@gmail.com>
2016-10-27 11:58:02 +0300

    add dgv on form for display matrix

---
+++ Automat/MainForm.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Automat
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTable = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTable
+            // 
+            this.dgvTable.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTable.Location = new System.Drawing.Point(12, 12);
+            this.dgvTable.Name = "dgvTable";
+            this.dgvTable.Size = new System.Drawing.Size(260, 237);
+            this.dgvTable.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.dgvTable);
+            this.Name = "MainForm";
+            this.Text = "o_O";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvTable;
+    }
+}
+
---
+++ Automat/MainForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Automat
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            MatrixToDgv(Calculator.GetMatrix(0.01, 0.1, 3));
+        }
+
+        public void MatrixToDgv(IEnumerable<IEnumerable<double>> data)
+        {
+            var matrix = data.Select(x => x.ToList()).ToList();
+
+            dgvTable.Rows.Clear();
+            dgvTable.Columns.Clear();
+
+            Enumerable.Range(0, matrix.Count > 0 ? matrix[0].Count : 0).ToList().ForEach(i => dgvTable.Columns.Add("", ""));
+
+            matrix.ForEach(row =>
+            {
+                var dgvRow = dgvTable.Rows.Add();
+                Enumerable.Range(0, row.Count).ToList().ForEach(cell => dgvTable[cell, dgvRow].Value = $"{row[cell]:0.####}");
+            });
+        }
+    }
+}
